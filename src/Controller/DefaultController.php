c05276bb4327d7ba178cc6848e4199f71f074262
jerp86 <jerp4@hotmail.com>
2021-01-04 14:15:06 -0300

    Add Usuario Controller in web and api

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,27 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
 class DefaultController
 {
-    /**
-     * @Route("/")
-     */
-    public function index(Request $request): Response
-    {
-        $resp = new Response();
+    // public function index(Request $request): Response
+    // {
+    //     $resp = new Response();
         
-        $resp->setContent(json_encode(
-            [
-                "recebido" => $request->get('nome'),
-                "ip" => $request->getClientIp()
-            ]
-        ));
-        $resp->setStatusCode(200);
+    //     $resp->setContent(json_encode(
+    //         [
+    //             "recebido" => $request->get('nome'),
+    //             "ip" => $request->getClientIp()
+    //         ]
+    //     ));
+    //     $resp->setStatusCode(200);
 
-        return $resp;
-    }
+    //     return $resp;
+    // }
 }
