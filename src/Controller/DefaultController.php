6cbaaaa9e830632efc9d22e173810eed5d6d1e09
jerp86 <jerp4@hotmail.com>
2021-01-04 13:38:28 -0300

    Utilizing json return

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController
 {
-    public function index()
+    public function index(Request $request)
     {
         $resp = new Response();
         
-        $resp->setContent('#DIO - Dgital Innovation One');
+        $resp->setContent(json_encode(
+            [
+                "recebido" => $request->get('nome'),
+                "ip" => $request->getClientIp()
+            ]
+        ));
         $resp->setStatusCode(200);
 
         return $resp;
