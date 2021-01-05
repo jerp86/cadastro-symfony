7c565dff546cee3b66c39b5f6906779ebaabd95c
Jose Eduardo Rodrigues Pinto (jerp86) <jerp4@hotmail.com>
2021-01-05 06:53:46 -0300

    Add Form template

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,21 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/", name="web_usuario_")
  */
-class UsuarioController
+class UsuarioController extends AbstractController
 {
     /**
      * @Route("/", methods={"GET"}, name="index")
      */
     public function index(): Response
     {
-        return new Response("implementar formulario de cadastro");
+        return $this->render("usuario/form.html.twig");
     }
 
     /**
