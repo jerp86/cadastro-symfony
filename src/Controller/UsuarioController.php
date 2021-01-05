e6ac97341040c10bb97950d6fc71be873d0515ca
jerp86 <jerp4@hotmail.com>
2021-01-05 16:25:18 -0300

    Add method salvar

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 // use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,8 +25,28 @@ class UsuarioController extends AbstractController
     /**
      * @Route("/salvar", methods={"POST"}, name="salvar")
      */
-    public function salvar(): Response
+    public function salvar(Request $request): Response
     {
-        return new Response("implementar gravação ao banco de dados");
+        $data = $request->request->all();
+
+        $usuario = new Usuario;
+
+        $usuario->setNome($data['nome']);
+        $usuario->setEmail($data['email']);
+
+        $doctrine = $this->getDoctrine()->getManager(); // pegar o gerenciador de entidades
+        $doctrine->persist($usuario); // prepara tudo para enviar ao banco
+        $doctrine->flush(); // faz com que a query (insert, update, delete, select) seja realizada no banco
+        
+        if ($doctrine->contains($usuario))
+        {
+            return $this->render("usuario/sucesso.html.twig", [
+                "fulano" => $data["nome"]
+            ]);
+        } else {
+            return $this->render("usuario/erro.html.twig", [
+                "fulano" => $data["nome"]
+            ]);
+        }
     }
 }
